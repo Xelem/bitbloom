caab2b75c61afa1b8c31a3017975913056196c89
Xelem <anselmchizurum@gmail.com>
2024-07-12 03:30:00 +0100

    Setup db, updated token sending

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,27 +1,27 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize')
 
 const devDB = {
-  username: "postgres",
-  password: process.env.POSTGRES_PASSWORD,
-  database: "coingrove",
-  host: "localhost",
-  dialect: "postgres",
-};
+    username: 'postgres',
+    password: process.env.POSTGRES_PASSWORD,
+    database: 'coingrove',
+    host: 'localhost',
+    dialect: 'postgres',
+}
 
 const sequelize = new Sequelize(
-  devDB.database,
-  devDB.username,
-  devDB.password,
-  devDB
-);
+    devDB.database,
+    devDB.username,
+    devDB.password,
+    devDB,
+)
 
 const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connected to database");
-  } catch (error) {
-    console.error("Unable to connect to the database", error);
-  }
-};
+    try {
+        await sequelize.authenticate()
+        console.log('Connected to database')
+    } catch (error) {
+        console.error('Unable to connect to the database', error)
+    }
+}
 
-module.exports = { connectDB, sequelize };
+module.exports = { connectDB, sequelize }
