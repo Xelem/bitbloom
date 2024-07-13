692d031d06d607471231715f8cdf6d955d778fa4
Xelem <anselmchizurum@gmail.com>
2024-07-13 21:23:55 +0100

    Fixed sidebar issue

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,107 +1,128 @@
-import React, { useEffect, useState } from "react";
-import { themeChange } from "theme-change";
-import { FaBell, FaMoon } from "react-icons/fa";
-import { HiBars3 } from "react-icons/hi2";
-import { MdSunny } from "react-icons/md";
-import Logo from "../assets/coingrove.png";
-import { FaUserCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { themeChange } from 'theme-change'
+import { FaBell, FaMoon } from 'react-icons/fa'
+import { HiBars3 } from 'react-icons/hi2'
+import { MdSunny } from 'react-icons/md'
+import Logo from '../assets/coingrove.png'
+import { FaUserCircle } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import LeftSidebar from './leftSidebar'
 
-function Header() {
-  const [currentTheme, setCurrentTheme] = useState(
-    localStorage.getItem("theme")
-  );
-
-  useEffect(() => {
-    themeChange(false);
-    if (currentTheme === null) {
-      if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        setCurrentTheme("dark");
-      } else {
-        setCurrentTheme("light");
-      }
+const Header: React.FC = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const toggleSidebar = () => {
+        setIsSidebarOpen(prev => !prev)
     }
-    // 👆 false parameter is required for react project
-  }, []);
 
-  function logoutUser() {
-    localStorage.clear();
-    window.location.href = "/";
-  }
+    const [currentTheme, setCurrentTheme] = useState(
+        localStorage.getItem('theme'),
+    )
 
-  return (
-    // navbar fixed  flex-none justify-between bg-base-300  z-10 shadow-md
+    useEffect(() => {
+        themeChange(false)
+        if (currentTheme === null) {
+            if (
+                window.matchMedia &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches
+            ) {
+                setCurrentTheme('dark')
+            } else {
+                setCurrentTheme('light')
+            }
+        }
+        // 👆 false parameter is required for react project
+    }, [])
 
-    <>
-      <div className="navbar fixed top-0 bg-base-100  z-10 shadow-md ">
-        {/* Menu toogle for mobile view or small screen */}
-        <div className="flex-1">
-          <label
-            htmlFor="left-sidebar-drawer"
-            className="btn bg-mainPurple dark:bg-deepPurple drawer-button lg:hidden"
-          >
-            <HiBars3 className="h-5 inline-block w-5 text-white" />
-          </label>
-          <div className="hidden md:flex space-x-2 items-center">
-            <img src={Logo} alt="logo" className="h-8 w-8" />
-            <h1 className="text-2xl text-gray-700 font-semibold ml-2 dark:text-gray-200">
-              CoinGrove
-            </h1>
-          </div>
-        </div>
+    function logoutUser() {
+        localStorage.clear()
+        window.location.href = '/'
+    }
 
-        <div className="flex-none">
-          {/* Light and dark theme selection toogle **/}
-          <label className="swap ">
-            <input type="checkbox" />
-            <FaMoon
-              data-set-theme="light"
-              data-act-class="ACTIVECLASS"
-              className={
-                "fill-current text-gray-700 w-6 h-6 " +
-                (currentTheme === "dark" ? "swap-on" : "swap-off")
-              }
-            />
-            <MdSunny
-              data-set-theme="dark"
-              data-act-class="ACTIVECLASS"
-              className={
-                "dark:text-gray-200 w-6 h-6 " +
-                (currentTheme === "light" ? "swap-on" : "swap-off")
-              }
-            />
-          </label>
+    return (
+        // navbar fixed  flex-none justify-between bg-base-300  z-10 shadow-md
 
-          {/* Notification icon */}
-          <button className="btn btn-ghost ml-4  btn-circle">
-            <FaBell className="text-gray-700 dark:text-gray-200 h-6 w-6" />
-          </button>
+        <>
+            <div className="navbar fixed top-0 bg-base-100  z-10 shadow-md ">
+                {/* Menu toogle for mobile view or small screen */}
+                <div className="flex-1">
+                    <LeftSidebar
+                        isOpen={isSidebarOpen}
+                        onClose={() => setIsSidebarOpen(false)}
+                    />
+                    <div className="flex-1">
+                        <label
+                            onClick={toggleSidebar}
+                            className="btn bg-mainPurple dark:bg-deepPurple drawer-button lg:hidden"
+                        >
+                            <HiBars3 className="h-5 inline-block w-5 text-white" />
+                        </label>
+                        <div className="hidden md:flex space-x-2 items-center">
+                            <img src={Logo} alt="logo" className="h-8 w-8" />
+                            <h1 className="text-2xl text-gray-700 font-semibold ml-2 dark:text-gray-200">
+                                CoinGrove
+                            </h1>
+                        </div>
+                    </div>
+                </div>
 
-          {/* Profile icon, opening menu on click */}
-          <div className="dropdown dropdown-end text-gray-700 dark:text-gray-200">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <FaUserCircle className="w-6 h-6" />
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li className="justify-between">
-                <Link to={"/user/profile"}>Profile Settings</Link>
-              </li>
-              <div className="divider mt-0 mb-0"></div>
-              <li>
-                <a onClick={logoutUser}>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                <div className="flex-none">
+                    {/* Light and dark theme selection toogle **/}
+                    <label className="swap ">
+                        <input type="checkbox" />
+                        <FaMoon
+                            data-set-theme="light"
+                            data-act-class="ACTIVECLASS"
+                            className={
+                                'fill-current text-gray-700 w-6 h-6 ' +
+                                (currentTheme === 'dark'
+                                    ? 'swap-on'
+                                    : 'swap-off')
+                            }
+                        />
+                        <MdSunny
+                            data-set-theme="dark"
+                            data-act-class="ACTIVECLASS"
+                            className={
+                                'dark:text-gray-200 w-6 h-6 ' +
+                                (currentTheme === 'light'
+                                    ? 'swap-on'
+                                    : 'swap-off')
+                            }
+                        />
+                    </label>
+
+                    {/* Notification icon */}
+                    <button className="btn btn-ghost ml-4  btn-circle">
+                        <FaBell className="text-gray-700 dark:text-gray-200 h-6 w-6" />
+                    </button>
+
+                    {/* Profile icon, opening menu on click */}
+                    <div className="dropdown dropdown-end text-gray-700 dark:text-gray-200">
+                        <label
+                            tabIndex={0}
+                            className="btn btn-ghost btn-circle avatar"
+                        >
+                            <FaUserCircle className="w-6 h-6" />
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            <li className="justify-between">
+                                <Link to={'/user/profile'}>
+                                    Profile Settings
+                                </Link>
+                            </li>
+                            <div className="divider mt-0 mb-0"></div>
+                            <li>
+                                <a onClick={logoutUser}>Logout</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Header;
+export default Header
--- frontend/src/components/leftSidebar.tsx
+++ frontend/src/components/leftSidebar.tsx
@@ -1,67 +1,64 @@
-import React, { useRef } from "react";
-import { NavLink, Link, useLocation } from "react-router-dom";
-import { AiOutlineClose } from "react-icons/ai";
-import { routes } from "../utils/routes";
-import SidebarSubmenu from "./sidebarSubmenu";
+import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import { AiOutlineClose } from 'react-icons/ai'
+import { routes } from '../utils/routes'
+import SidebarSubmenu from './sidebarSubmenu'
 
-const LeftSidebar = () => {
-  const location = useLocation();
-  const leftSidebarDrawerRef = useRef<HTMLDivElement>(null);
+interface LeftSidebarProps {
+    isOpen: boolean
+    onClose: () => void
+}
 
-  const close = () => {
-    const leftSidebarDrawer = leftSidebarDrawerRef.current;
-    if (leftSidebarDrawer) {
-      leftSidebarDrawer.click();
-    }
-  };
+const LeftSidebar: React.FC<LeftSidebarProps> = ({ isOpen, onClose }) => {
+    const location = useLocation()
 
-  return (
-    <div className={`z-30 h-[100%] fixed`}>
-      <ul className="menu pt-2 w-80 bg-base-100 min-h-full text-base-content">
-        <button
-          className="btn btn-ghost bg-base-300  btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden"
-          onClick={() => close()}
+    return (
+        <div
+            className={`z-30 h-[8%]  fixed transition-transform ${
+                isOpen ? '-translate-x-2' : '-translate-x-full'
+            }`}
         >
-          <AiOutlineClose className="h-5 inline-block w-5" />
-        </button>
-
-        <li className="mb-2 font-semibold text-xl md:hidden">
-          <Link to={"/app/welcome"}>
-            <p>Logo</p>
-          </Link>{" "}
-        </li>
-        {routes.map((route, k) => {
-          return (
-            <li className="" key={k}>
-              {route.submenu ? (
-                <SidebarSubmenu {...route} />
-              ) : (
-                <NavLink
-                  end
-                  to={route.path}
-                  className={({ isActive }) =>
-                    `${
-                      isActive
-                        ? "font-semibold  bg-base-200 text-lg"
-                        : "font-normal"
-                    }`
-                  }
+            <ul className="menu pt-2 h-screen w-80 bg-base-100 min-h-full text-base-content">
+                <button
+                    className="btn btn-ghost bg-base-300 btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden"
+                    onClick={onClose}
                 >
-                  {route.icon} {route.name}
-                  {location.pathname === route.path ? (
-                    <span
-                      className="absolute inset-y-0 left-0 w-1 rounded-tr-md rounded-br-md bg-primary "
-                      aria-hidden="true"
-                    ></span>
-                  ) : null}
-                </NavLink>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
+                    <AiOutlineClose className="h-5 inline-block w-5" />
+                </button>
 
-export default LeftSidebar;
+                <li className="mb-2 font-semibold text-xl md:hidden">
+                    <p>CoinGrove</p>
+                </li>
+                {routes.map((route, k) => (
+                    <li key={k}>
+                        {route.submenu ? (
+                            <SidebarSubmenu {...route} />
+                        ) : (
+                            <NavLink
+                                end
+                                to={route.path}
+                                className={({ isActive }) =>
+                                    `${
+                                        isActive
+                                            ? 'font-semibold bg-base-200 text-lg'
+                                            : 'font-normal'
+                                    }`
+                                }
+                            >
+                                {route.icon} {route.name}
+                                {location.pathname === route.path ? (
+                                    <span
+                                        className="absolute inset-y-0 left-0 w-1 rounded-tr-md rounded-br-md bg-primary"
+                                        aria-hidden="true"
+                                    ></span>
+                                ) : null}
+                            </NavLink>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default LeftSidebar
